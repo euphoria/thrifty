bb7940d50c821dd0b1061a0a9a69017a59cde7cd
Michael Greene <michael.greene@gmail.com>
2009-03-07 14:31:29 -0600

    Initial setup of repository, README, and setuptools installation

---
+++ README.rst
@@ -0,0 +1,23 @@
+===========
+``thrifty``
+===========
+
+``thrifty`` is a Python-based generator for Apache Thrift source files.
+
+You can get ``thrifty`` via ``git``::
+
+    git clone git://github.com/euphoria/thrifty
+
+And install it via::
+
+    python setup.py install
+
+``thrifty`` can also be conveniently installed from PyPI_ using ``easy_install``::
+
+    easy_install -U thrifty
+
+Usage
+=====
+
+``thrifty`` is invoked from the command-line and accepts similar commands as
+the original Apache Thrift compiler.
---
+++ setup.py
@@ -0,0 +1,29 @@
+try:
+    from setuptools import setup, find_packages
+except:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name='thrifty',
+    version='0.1',
+    description='thrifty is a Python-based generator for Apache Thrift source',
+    long_description=open('README.rst').read(),
+    keywords='thrift parser generator',
+    license='Apache Software License',
+    author='Michael Greene',
+    author_email='michael.greene@gmail.com',
+    url='http://github.com/euphoria/thrifty/',
+    dependency_links=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Code Generators',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
