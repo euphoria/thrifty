32950cea3402ab70eec72e4ff8fc74f3a26ba398
Michael Greene <michael.greene@gmail.com>
2009-03-07 19:57:05 -0600

    Command line tool ++, still has virtually no functionality

--- thrifty/core/runner.py
+++ thrifty/core/runner.py
@@ -4,12 +4,17 @@ from thrifty.gen import generator
 
 def execute():
     parser = OptionParser()
-    parser.add_option('--gen', action='append', dest='generators',
+    parser.add_option('-g', '--gen',
+        action='append', dest='generators', metavar='GENERATOR',
         help='Generate code with a dynamically-registered generator')
     parser.add_option('-v', action='store_true', dest='verbose',
         help='Verbose mode')
+    parser.add_option('-I', '--include',
+        action='append', dest='includes', metavar='DIR',
+        help='Add a directory to the list of directories searched for include directives')
 
     (options, args) = parser.parse_args()
 
-    for gen in options.generators:
-        print gen
+    if options.generators:
+        for gen in options.generators:
+            print gen
